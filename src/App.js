f5d1487f2247afb9d20cb6f20db0c361e4276121
nlindfors <niklas.lindfors@hotmail.com>
2022-12-02 11:46:29 +0100

    uppdaterade slumpad ordning på svarsalternativ

--- src/App.js
+++ src/App.js
@@ -80,13 +80,36 @@ function App() {
     }
   }
 
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
+  
+  const options = q[index].incorrect_answers.concat(q[index].correct_answer)
+  shuffle(options);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="div__category">{q[index].category}</div>
         <div className="div__question">{q[index].question}</div>
         <div className="div__answers">
-          {q[index].incorrect_answers.concat(q[index].correct_answer).map((answer) => (
+          {options.map((answer) => (
             <button value={answer} onClick={checkAnswer}>
               {answer}
             </button>
